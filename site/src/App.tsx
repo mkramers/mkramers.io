fd8ea20dc4b470a434ad9cbfd732e0fc99f34e63
mkramers <mattkramers@gmail.com>
2020-03-15 18:16:12 -0400

    Add navbar

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Alignment, Button, Classes, Navbar, NavbarDivider, NavbarGroup, NavbarHeading} from "@blueprintjs/core";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello mkramers</h1>
+    <div style={{background: 'red'}}>
+        <Navbar>
+            <NavbarGroup align={Alignment.LEFT}>
+                <NavbarHeading>mkramers</NavbarHeading>
+                <NavbarDivider />
+                <Button className={Classes.MINIMAL} icon="home" text="Home" />
+                <Button className={Classes.MINIMAL} icon="document" text="Files" />
+            </NavbarGroup>
+        </Navbar>
     </div>
   );
 }
