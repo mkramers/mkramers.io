6177cf04227d45704e5f8f22f8a5d7be2fcb8ccc
mkramers <mattkramers@gmail.com>
2020-03-22 21:30:04 -0400

    Add create post button

--- site/src/components/NavBar.tsx
+++ site/src/components/NavBar.tsx
@@ -8,6 +8,7 @@ function NavBar() {
                 <NavbarHeading>mkramers.io</NavbarHeading>
                 <NavbarDivider/>
                 <Button className={Classes.MINIMAL} icon="home" text="Home"/>
+                <Button className={Classes.MINIMAL} icon="new-text-box" text="Create Post"/>
             </NavbarGroup>
         </Navbar>
     );
