a664b4a34a2da1cdfbbb203c6eb0011ae9616bc9
mkramers <mattkramers@gmail.com>
2020-03-28 22:33:04 -0400

    Convert to function component

--- site/src/components/PostList.tsx
+++ site/src/components/PostList.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Classes, ITreeNode, Tree} from "@blueprintjs/core";
 import {Post} from "../types/Post";
 import {connect} from "react-redux";
 import {State} from "../store";
 import {selectPost} from "../store/posts/actions";
-
-export interface PostListState {
-    nodes: ITreeNode[];
-}
 
 type PostListProps = {
     posts: Post[],
@@ -42,56 +38,54 @@ let getPostNode = (post: Post) => {
     return postTreeNode;
 };
 
-export class PostList extends React.Component<PostListProps, PostListState> {
-    state: PostListState = {
-        nodes: transformPosts(this.props.posts, this.props.selectedPostId)
-    };
+function PostList({posts, selectedPostId, selectPost}: PostListProps) {
+    const [nodes, setNodes] = useState<ITreeNode[]>([]);
 
-    handleNodeClick = (nodeData: ITreeNode, _nodePath: number[], e: React.MouseEvent<HTMLElement>) => {
+    useEffect(() => {
+        let nodes = transformPosts(posts, selectedPostId);
+        setNodes(nodes);
+    }, [posts, selectedPostId]);
+
+    let handleNodeClick = (nodeData: ITreeNode, _nodePath: number[], e: React.MouseEvent<HTMLElement>) => {
         const originallySelected = nodeData.isSelected;
         if (!e.shiftKey) {
-            if (this.state?.nodes) {
-                this.forEachNode(this.state.nodes, n => (n.isSelected = false));
+            if (nodes) {
+                forEachNode(nodes, n => (n.isSelected = false));
             }
         }
         nodeData.isSelected = originallySelected == null ? true : !originallySelected;
-        this.setState(this.state);
 
-        this.props.selectPost(Number(nodeData.id));
+        selectPost(Number(nodeData.id));
     };
 
-    handleNodeCollapse = (nodeData: ITreeNode) => {
+    let handleNodeCollapse = (nodeData: ITreeNode) => {
         nodeData.isExpanded = false;
-        this.setState(this.state);
     };
 
-    handleNodeExpand = (nodeData: ITreeNode) => {
+    let handleNodeExpand = (nodeData: ITreeNode) => {
         nodeData.isExpanded = true;
-        this.setState(this.state);
     };
 
-    forEachNode = (nodes: ITreeNode[] | undefined, callback: (node: ITreeNode) => void) => {
+    let forEachNode = (nodes: ITreeNode[] | undefined, callback: (node: ITreeNode) => void) => {
         if (nodes == null) {
             return;
         }
 
         for (const node of nodes) {
             callback(node);
-            this.forEachNode(node.childNodes, callback);
+            forEachNode(node.childNodes, callback);
         }
     };
 
-    render() {
-        return (
-            <Tree
-                contents={this.state.nodes}
-                onNodeClick={this.handleNodeClick}
-                onNodeCollapse={this.handleNodeCollapse}
-                onNodeExpand={this.handleNodeExpand}
-                className={Classes.ELEVATION_0}
-            />
-        );
-    }
+    return (
+        <Tree
+            contents={nodes}
+            onNodeClick={handleNodeClick}
+            onNodeCollapse={handleNodeCollapse}
+            onNodeExpand={handleNodeExpand}
+            className={Classes.ELEVATION_0}
+        />
+    );
 }
 
 const mapState = (state: State) => ({
