2f774a376c6b301b1625a520dabce11f8799c8e6
mkramers <mattkramers@gmail.com>
2020-03-29 07:47:15 -0400

    Add fields to create post

--- site/src/components/CreatePost.tsx
+++ site/src/components/CreatePost.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from "react-redux";
-import "./MainPage.css";
+import "./CreatePost.css";
+import {Button, Card, Elevation, InputGroup, TextArea} from "@blueprintjs/core";
+import {Intent} from "@blueprintjs/core/lib/esm/common/intent";
 
-type CreatePostProps = {
-};
+type CreatePostProps = {};
 
 function CreatePost({}: CreatePostProps) {
+    const [title, setTitle] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+
+    let handleSubmit = () => {
+
+    };
+
     return (
-        <div className="main">
-            Create!
-        </div>
+        <Card interactive={true} elevation={Elevation.TWO} className="main">
+            <h5>Create post</h5>
+            <InputGroup
+                onChange={(e: any) => setTitle(e.target.value)}
+                placeholder="Title"
+                value={title}
+            />
+            <TextArea
+                className="text-area"
+                intent={Intent.PRIMARY}
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+            />
+            <Button onClick={handleSubmit}>Submit</Button>
+        </Card>
     );
 }
 
-const mapState = () => ({
-});
+const mapState = () => ({});
 
-const mapDispatch = {
-};
+const mapDispatch = {};
 
 const connector = connect(mapState, mapDispatch);
 
